d80f76d2fced3615a13b76d0e972faf78cf2288f
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-10-26 11:25:49 -0600

    Errores en app.component y app-module

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,35 +1,29 @@
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from '../app/infrastructure/driving-adapters/app/app.component';
-import { PlaylistViewComponent } from '../app/infrastructure/driving-adapters/components/pages/playlist-view.component';
-
-import { PlayerControlsComponent } from '../app/infrastructure/driving-adapters/components/layout/player-controls.component'; 
-import { SidebarComponent } from '../app/infrastructure/driving-adapters/components/layout/sidebar.component'; 
-
-import { IMusicRepository } from './core/domain/ports/out/i-music.repository';
-import { IPlayerUseCases } from './core/domain/ports/in/i-player.use-cases';
-import { SpotifyService } from './infrastructure/driven-adapters/spotify-api/spotify.service';
+ 
+import { IMusicRepository } from '../app/core/domain/ports/out/i-music.repository';
+import { IPlayerUseCases } from '../app/core/domain/ports/in/i-player.use-cases';
+import { SpotifyService } from '../app/infrastructure/driven-adapters/spotify-api/spotify.service';
 import { PlayerService } from './core/application/player.service';
 import { SpotifyInterceptor } from './infrastructure/driven-adapters/spotify-api/spotify.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing-module';
+import { AppComponent } from './infrastructure/driving-adapters/app/app.component';
+import { PlayerControlsComponent } from './infrastructure/driving-adapters/components/layout/player-controls.component';
+import { SidebarComponent } from './infrastructure/driving-adapters/components/layout/sidebar.component';
+import { PlaylistViewComponent } from './infrastructure/driving-adapters/components/pages/playlist-view.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PlaylistViewComponent,
-    
-  
     PlayerControlsComponent,
-    SidebarComponent       
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-
     AppRoutingModule,
-
     HttpClientModule
   ],
   providers: [
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// Importa la página que quieres mostrar
+import { PlaylistViewComponent } from '../app/infrastructure/driving-adapters/components/pages/playlist-view.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Esto le dice al <router-outlet> que cargue PlaylistViewComponent por defecto
+  { path: '', component: PlaylistViewComponent },
+  
+  // Puedes añadir más rutas (como /buscar) aquí en el futuro
+  // { path: 'buscar', component: SearchComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
