7d7c7365f78a9224fd747a59aa9f99db57d83aba
AyelenRod <243681@ids.upchiapas.edu.mx>
2025-12-08 14:46:40 -0600

    si

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  standalone: false // ← Asegúrate que está en false
+  standalone: false 
 })
 export class App {
-  // Ya no necesitas el título si no lo usas
+ 
 }
